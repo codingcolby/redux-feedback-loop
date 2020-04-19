d5a6cfde9d1fcaa6c833d91286869eedaccb9d43
allthethings <learn2code@respectdesfonds.com>
2020-04-19 00:04:24 -0500

    installed axios, react, react-router-dom and established paths for each page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
+import { hashRouter as Router, Route, Link } from 'react-router-dom';
+import FeelingPage from '../Pages/FeelingPage/FeelingPage';
+import UnderstandingPage from '../Pages/UnderstandingPage/UnderstandingPage';
+import SupportPage from '../Pages/SupportPage/SupportPage';
+import CommentPage from '../Pages/CommentPage/CommentPage';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<Router>
+					<header className='App-header'>
+						<h1 className='App-title'>Feedback!</h1>
+						<h4>
+							<i>Don't forget it!</i>
+						</h4>
+					</header>
+					<br />
+					<Route exact path='/' component={FeelingPage} />
+					<Route exact path='/' component={UnderstandingPage} />
+					<Route exact path='/' component={SupportPage} />
+					<Route exact path='/' component={CommentPage} />
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
