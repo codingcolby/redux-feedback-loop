7354acefbf8c957b706c93877f07e5cdf0074400
allthethings <learn2code@respectdesfonds.com>
2020-04-18 23:21:42 -0500

    dpg declarations and config for database connection

--- server/pool.js
+++ server/pool.js
@@ -1,1 +1,23 @@
 const pg = require('pg');
+const Pool = pg.Pool;
+
+const config = {
+	database: 'prime_feedback',
+	host: 'localhost',
+	port: 5432,
+	max: 10,
+	idleTimeoutMillis: 26000,
+};
+
+pool = new Pool(config);
+
+pool.on('connect', (client) => {
+	console.log(`Successful connection to the ${database} database`);
+});
+
+pool.on('error', (err, client) => {
+	console.log('Error on idle pg client', err);
+	process.exit(-1);
+});
+
+module.exports = pool;
