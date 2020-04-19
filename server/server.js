b5eb27bf4bee5a29c689e14e2395084fd5a10d4d
allthethings <learn2code@respectdesfonds.com>
2020-04-19 14:05:38 -0500

    addition of routes

--- server/server.js
+++ server/server.js
@@ -3,15 +3,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
+const feedbackRouter = require('./routes/feedback.router');
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.use('/feedback', feedbackRouter);
+app.use('/*', (req, res) => {
+	console.log('Here you are');
+	res.sendStatus(200);
+});
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
-    console.log('Listening on port: ', PORT);
+	console.log(`Listening on port: ${PORT}`);
 });
