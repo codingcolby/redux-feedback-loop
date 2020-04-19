584214637d80552b21b99d03cd24e8a8bdfe42df
allthethings <learn2code@respectdesfonds.com>
2020-04-19 12:10:37 -0500

    built out  imports, class, methoods, and render div with input and button

---
+++ src/components/App/Pages/FeelingPage/FeelingPage.js
@@ -0,0 +1,51 @@
+import React, { Components } from 'react';
+import { connect } from 'react-redux';
+import { render } from 'react-dom';
+
+class FeelingPage extends Component {
+	state = {
+		feeling: '',
+	};
+
+	onInputChange = (input) => (event) => {
+		this.setState(
+			{
+				[input]: event.target.value,
+			},
+			() => {
+				console.log(this.state);
+			}
+		);
+	};
+
+	onNextClick = (event) => {
+		this.props.dispatch({ type: 'SET-FEELING', payload: this.state });
+		this.props.history.push('/understanding');
+	};
+
+	render() {
+		console.log(this.props.store);
+
+		return (
+			<div>
+				<h2>How are you feeling today?</h2>
+				<p>
+					Please select a value between 1-5 with 1 = not so good and 5 =
+					fantastic
+				</p>
+				<input
+					type='number'
+					required
+					onChange={this.onInputChange('feedback')}
+					placeholder='Rank your feeling'
+				/>
+				)}
+				<button onClick={this.onNextClick}>Next</button>
+			</div>
+		);
+	}
+}
+
+const mapStoreToProps = (store) => ({ store });
+
+export default connect(mapStoreToProps)(FeelingPage);
