7c0506f1eaf721d05edbab6e0b8639e4c7834049
allthethings <learn2code@respectdesfonds.com>
2020-04-19 15:53:37 -0500

    added reducers and additional imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feelingReducer = (state = '', action) => {
+	if (action.type === 'SET_FEELING') {
+		return action.payload;
+	}
+	return state;
+};
+
+const understandingReducer = (state = '', action) => {
+	if (action.type === 'SET_UNDERSTANDING') {
+		return action.payload;
+	}
+	return state;
+};
+
+const supportReducer = (state = '', action) => {
+	if (action.type === 'SET_SUPPORT') {
+		return action.payload;
+	}
+	return state;
+};
+
+const commentsReducer = (state = '', action) => {
+	if (action.type === 'SET_COMMENTS') {
+		return action.payload;
+	}
+	return state;
+};
+
+const storeInstance = createStore(
+	combineReducers({
+		feelingReducer,
+		understandingReducer,
+		supportReducer,
+		commentsReducer,
+	}),
+	applyMiddleware(logger)
+);
+
+ReactDOM.render(
+	<Provider store={storeInstance}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
